7c0734737f6e05028535d8a9244aa3fecb90496c
Agurtzane <aurtizberea@azti.es>
2024-07-23 17:29:00 +0200

    cleaning code

--- code/analysingModels/Analysis_4ss3_outputs.R
+++ code/analysingModels/Analysis_4ss3_outputs.R
@@ -15,7 +15,7 @@ library(ggplot2)
 proj_dir = here::here()
 setwd(proj_dir)
 
-source("code/analysingModels/auxiliary_functions_4analysis.R")
+source("code/analysingModels/auxiliary_functions_4analysingModels.R")
 # Sharepoint path:
 source('sharepoint_path.R')
 setwd(shrpoint_path)
@@ -31,8 +31,8 @@ dir_table <- "output/tables/"
 #...................................................
 
 
-scs <- c("4A_io_2021_v33017","4A_io_win_v330221","4A_io_lin_v330221")  #"4A_io_lin_v33021",
-desc <- c("BaseCase_2021", "BaseCase_V3.30.22.1","BaseCase_linux_v3.30.22.1")  #"BaseCase_linux_v3.30.21",
+scs <- c("4A_io_2021_v33017","4A_io_win_v330221","4A_io_lin_v330221")  
+desc <- c("BaseCase_2021", "BaseCase_V3.30.22.1","BaseCase_linux_v3.30.22.1")  
 scs_wd <-paste0("models/base_win_vs_lin/",scs)
 
 plot_ss3 <- FALSE
@@ -40,7 +40,8 @@ add_perf_table <- TRUE
 
 for(i in 1:length(scs)){
   sc_ss3 <- SS_output(dir=scs_wd[i],  repfile = "Report.sso",covar=F)
-  if (plot_ss3) { SS_plots(sc_ss3,uncertainty=T,png=F,forecastplot=F, fitrange = TRUE, parrows=5, parcols=4, showdev= TRUE) }
+  if (plot_ss3) { SS_plots(sc_ss3,uncertainty=T,png=T,forecastplot=F, fitrange = TRUE, 
+                           parrows=5, parcols=4, showdev= TRUE) }
   if(add_perf_table){
   summary_file <- paste0(dir_table,"summaryPerformance.csv")
   df <-mod.performance(scs[i],desc[i],sc_ss3,summary_file)
@@ -67,7 +68,7 @@ df_long$scenario <- as.factor(as.character(df_long$scenario))
 
 
 ggplot(df_long,aes(x=Yr,y=value,group =scenario,colour = scenario))+geom_point( size = 1)+
-  geom_line( )+xlim(200,298)+# geom_smooth(method = "loess")+
+  geom_line( )+xlim(200,298)+
   scale_color_discrete(labels=levels(df_long$scenario))+
   xlab("Year")+ ylab("F/Fmsy")+theme_fun()
  
@@ -84,7 +85,7 @@ df <- df %>% rename_at(1:length(scs),~scs)
 df_long <- df %>% pivot_longer(cols =scs, names_to = "scenario", values_to = "value")
 
 ggplot(df_long,aes(x=Yr,y=value,group =scenario,colour = scenario))+geom_point(   size = 1)+
-  geom_line( )+xlim(200,298)+ylim(0,2.5e6)+# geom_smooth(method = "loess")+
+  geom_line( )+xlim(200,298)+ylim(0,2.5e6)+
   scale_color_discrete(labels=scs)+xlab("Year")+ ylab("SSB")+
   theme_fun()
 
