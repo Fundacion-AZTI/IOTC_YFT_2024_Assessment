333d1bb1840eb7b4c28fb20e377d76d7b90dc0c9
Giancarlo M. Correa <g.moroncorrea@gmail.com>
2024-08-05 21:58:02 +0200

    add cpue image

--- code/data_exploration/input_ts.R
+++ code/data_exploration/input_ts.R
@@ -1,6 +1,7 @@
 # Read path and parameters for plots:
 source('sharepoint_path.R')
 source(here('code/data_exploration', 'parameters_for_plots.R'))
+source(here('code/data_exploration', 'auxiliary_functions.R'))
 
 # Catch per fleet as barplot ----------------------------------------------
 
@@ -42,6 +43,31 @@ p1 = ggplot(data = plot_data, aes(x = year, y = catch, fill = fisherycode)) +
   guides(fill = guide_legend(title = NULL)) +
   facet_wrap( ~ area)
 ggsave(file.path(shrpoint_path, plot_dir, paste0('ts_catch_area', img_type)), plot = p1,
+       width = 170, height = 130, units = 'mm', dpi = img_res)
+
+
+# CPUE with CV ------------------------------------------------------------
+
+spat_config = '4A_io'
+cpue_dat = read_csv(file.path(shrpoint_path, 'data/ss3_inputs', spat_config, 'scaled_cpue_Meancv_02.csv'))
+cpue_dat = cpue_dat %>% mutate(area = if_else(fleet == 22, '1b', 
+                                              if_else(fleet == 23, '2', 
+                                                      if_else(fleet == 24, '3', '4'))))
+cpue_dat = cpue_dat %>% mutate(time = ssts2yq(qtr))
+cpue_dat = cpue_dat %>% dplyr::rename(obs = pr_7994_m8)
+cpue_dat = cpue_dat %>% mutate(sd = obs*cv)
+
+# Plot data:
+plot_data = cpue_dat
+plot_data$area = factor(plot_data$area, levels = c('1b', '4', '2', '3'))
+
+p1 = ggplot(data = plot_data, aes(x = time, y = obs)) +
+  geom_ribbon(aes(ymin = obs - sd, ymax = obs + sd), fill = 'grey70') +
+  geom_line(aes(y = obs)) +
+  coord_cartesian(ylim = c(0, NA)) + 
+  xlab(NULL) + ylab("Scaled CPUE") +
+  facet_wrap( ~ area, scales = 'free_y')
+ggsave(file.path(shrpoint_path, plot_dir, paste0('ts_cpue_area', img_type)), plot = p1,
        width = 170, height = 130, units = 'mm', dpi = img_res)
 
 
