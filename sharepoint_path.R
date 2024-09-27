14e8db0b0cdb210db5715d7a436ee03642acc918
Giancarlo M. Correa <g.moroncorrea@gmail.com>
2024-09-27 11:01:08 +0200

    delete sharepoint to add to gitignore

--- sharepoint_path.R
+++
@@ -1,4 +0,0 @@
-# Specify your Sharepoint local path, where the data and SS configurations are saved. 
-# IMPORTANT: never commit/push changes in this R script
-
-shrpoint_path = 'D:/OneDrive - AZTI/General - IOTC_YFT_2024'
